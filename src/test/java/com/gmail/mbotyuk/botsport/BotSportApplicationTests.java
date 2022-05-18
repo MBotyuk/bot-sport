ef9b159edb9559110f5b685e3cc012612a20fa58
18725902 <sigmaSBER2204>
2022-05-18 22:07:05 +0300

    init

---
+++ src/test/java/com/gmail/mbotyuk/botsport/BotSportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.mbotyuk.botsport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotSportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
